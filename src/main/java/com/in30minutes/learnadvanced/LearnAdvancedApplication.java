d002f837782e56f908c19ce6efc29a0559fe705d
Mercia Jeno <mercia.jeno@gmail.com>
2023-12-14 09:12:19 +0530

    Initial push

---
+++ src/main/java/com/in30minutes/learnadvanced/LearnAdvancedApplication.java
@@ -0,0 +1,13 @@
+package com.in30minutes.learnadvanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnAdvancedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnAdvancedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/in30minutes/learnadvanced/LearnAdvancedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in30minutes.learnadvanced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnAdvancedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
